37f680adffe0c773c279b1bce846c9f8c3066bd3
neppoliean <neppoliean.joseph@aspiresys.com>
2018-02-17 04:42:02 +0530

    First commit

---
+++ test/Test/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        body
+        {
+            font-family: Arial;
+            font-size: 10pt;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+<asp:GridView ID="GridView1" HeaderStyle-BackColor="#3AC0F2" HeaderStyle-ForeColor="White"
+    RowStyle-BackColor="#A1DCF2" AlternatingRowStyle-BackColor="White" AlternatingRowStyle-ForeColor="#000"
+    runat="server" AutoGenerateColumns="false">
+    <Columns>
+      <asp:BoundField DataField="ID" HeaderText="ID" ItemStyle-Width="150" />
+        <asp:BoundField DataField="Name" HeaderText="Name" ItemStyle-Width="150" />
+        <asp:BoundField DataField="Country" HeaderText="Country" ItemStyle-Width="150" />
+    </Columns>
+</asp:GridView>
+    </form>
+</body>
+</html>
---
+++ test/Test/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+	DataTable dt = new DataTable();
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		if (Request.QueryString != null)
+		{
+			if (!IsPostBack)
+			{
+
+
+			}
+			if (Request.QueryString["ID"] != "")
+			{
+				int ReqID = 0;
+				ReqID = Convert.ToInt32(Request.QueryString["ID"].ToString());
+				string strQuery = "";
+				string id = Convert.ToString(ReqID);
+				if (id != "0")
+				{
+					strQuery = "select * from [Table] WHERE ID =" + id + "";
+					SqlCommand cmd = new SqlCommand(strQuery);
+					dt = GetData(cmd);
+					GridView1.DataSource = dt;
+					GridView1.DataBind();
+				}
+			}
+		}
+			
+	}
+	private DataTable GetData(SqlCommand cmd)
+	{
+		DataTable dt = new DataTable();
+		String strConnString = System.Configuration.ConfigurationManager.ConnectionStrings["SQL"].ConnectionString;
+		SqlConnection con = new SqlConnection(strConnString);
+		SqlDataAdapter sda = new SqlDataAdapter();
+		cmd.CommandType = CommandType.Text;
+		cmd.Connection = con;
+		try
+		{
+			con.Open();
+			sda.SelectCommand = cmd;
+			sda.Fill(dt);
+
+		}
+		catch (Exception ex)
+		{
+			throw ex;
+			//return null;
+		}
+		finally
+		{
+			con.Close();
+			sda.Dispose();
+			con.Dispose();
+		}
+		return dt;
+	}
+}
